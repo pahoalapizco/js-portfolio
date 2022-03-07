1fe1f038dfc1b5140701afe0f9f1058513916fbf
Paola Alapizco <paolac.alapizco@gmail.com>
2022-03-06 18:50:28 -0700

    Integracion de css y stylus loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const ruleForJavaScript = {
+  test: /\.m?js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: "babel-loader",
+  },
+};
+
+const ruleForCss = {
+  test: /\.css|.styl$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    "css-loader",
+    "stylus-loader"
+  ],
+};
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: 'main.js',
+    filename: "main.js",
   },
   resolve: {
     extensions: [".js"],
   },
   module: {
     rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
+      ruleForJavaScript,
+      ruleForCss,
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
       template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
