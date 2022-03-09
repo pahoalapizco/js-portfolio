ac7104b542f23fde5e15e15c08bd405c571e4ae9
Paola Alapizco <paolac.alapizco@gmail.com>
2022-03-09 08:07:17 -0700

    Console del error

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
 
-fs.writeFile('./.env', `${API=process.env.API}`, () => {});
+fs.writeFile('./.env', `${API=process.env.API}`, (err) => {
+  if(err){
+    onsole.log("🚀 ~ file: create-env.js ~ line 5 ~ fs.writeFile ~ err", err)
+  }
+});
